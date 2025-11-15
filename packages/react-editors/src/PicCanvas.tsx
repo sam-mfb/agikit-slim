6d34a1bf525b778b9eb0e87d729891daed97b734
Sam Davidoff <sam@mfbtech.com>
2025-11-15 21:56:02 +0000

    Phase 1: Rush.js setup - remove Yarn, create Rush structure
    - Remove Yarn artifacts (.yarn/, .pnp.*, yarn.lock, .yarnrc.yml)
    - Remove unwanted packages (vscode, language-server, react-editors)
    - Create Rush.js configuration (rush.json)
    - Create pnpm configuration (.npmrc, .pnpmfile.cjs)
    - Reduce from 5 packages to 2 (core + cli)

--- packages/react-editors/src/PicCanvas.tsx
+++
@@ -1,8 +0,0 @@
-import { PIC_HEIGHT, PIC_WIDTH } from './constants';
-import { DrawingCanvas } from './DrawingCanvas';
-
-export function PicCanvas(
-  props: Omit<Parameters<typeof DrawingCanvas>[0], 'sourceWidth' | 'sourceHeight'>,
-) {
-  return <DrawingCanvas {...props} sourceWidth={PIC_WIDTH} sourceHeight={PIC_HEIGHT} />;
-}
--- packages/react-editors/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const PIC_WIDTH = 160;
-export const PIC_HEIGHT = 168;
-export const DISPLAY_ASPECT_RATIO = (PIC_WIDTH * 2) / PIC_HEIGHT;
